b8fc78d7f5554fc35b59376fc4eb582082096dcb
mischa talokonnikoff <mischatalokonnikoff@gmail.com>
2018-01-17 03:10:22 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,3 @@
-def display_rainbow
-end
-display_rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+def display_rainbow(carr)
+   puts "R: #{carr[0]}, O: #{carr[1]}, Y: #{carr[2]}, G: #{carr[3]}, B: #{carr[4]}, I: #{carr[5]}, V: #{carr[6]}"
+ end
